7a0c142446458780621c27ca026e606a3de37f44
Chase Abrahams <cabrahams317@gmail.com>
2020-08-19 02:02:16 +0200

    blog should be fixed

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class BlogController extends Controller
 {
   public function show($id) {
-    $blog = Blog::where('id', '=', $id)->first()->get();
+    $blog = Blog::where('id', '=', $id)->first();
     $comments = Comment::where('blog_id', '=', $id)->orderBy('created_at', 'desc')->get();
     return view('blog', ['blog' => $blog, 'comments' => $blog]);
   }
